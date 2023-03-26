766c2b6e7ab5d54d54cc854934a88bf538d2b136
Mark <markjackson2112@gmail.com>
2023-03-26 17:00:10 -0400

    HIB-1 implemented persistent storage rather than heap storage

--- hi.js
+++ hi.js
@@ -1,29 +1,31 @@
+var db = require("./db");
+
 const DAY_IN_MILLIS = 86400000;
+const pingArray = Object.create(null);
 
-var hiChannel = null;
-var lastUser = 0;
-var lastHi = Date.now();
-var nextHi = Date.now();
-var pingHi = null//setImmediate(function () {});
-
-function checkHi(msg, client){
+async function checkHi(msg, client){
     if(msg.channel.name == "hi"){
-        var botControlChannel = msg.guild.channels.cache.find(channel => channel.name === "bot-control")
         //check that the message was sent to #hi
+
+        //get data from DB
+        var data = await db.getHi(msg.guild.id);
+        var botControlChannel = msg.guild.channels.cache.find(channel => channel.id === data.botControl)
+
+        //log and process hi
         console.log("[" + msg.createdTimestamp + "]: \"" + msg.content + "\" from " + msg.member.user.tag)
         if(msg.content == "hi"){
             //message is hi
-            console.log("  next \"hi\" expected at " + nextHi)
-            if(msg.createdTimestamp < nextHi){
+            console.log("  next \"hi\" expected at " + data.nextHi)
+            if(msg.createdTimestamp < data.nextHi){
                 //not long enough since last hi
                 console.log("  not long enough since last \"hi\"")
                 console.log("  deleting \"hi\"")
                 msg.delete();
                 //inform user why their hi is bad
-                botControlChannel.send("Sorry <@" + msg.member.user.id + ">, you tried to say hi too soon :frowning: \nThe next hi can occur in " + msToTime(nextHi - msg.createdTimestamp));
+                botControlChannel.send("Sorry <@" + msg.member.user.id + ">, you tried to say hi too soon :frowning: \nThe next hi can occur in " + msToTime(data.nextHi - msg.createdTimestamp));
             } else{
                 //24 hours since last hi
-                if(msg.member.user.id == lastUser){
+                if(msg.member.user.id == data.lastUser){
                     //invalid hi
                     console.log("  same user as last \"hi\"")
                     console.log("  deleting \"hi\"")
@@ -34,19 +36,20 @@ function checkHi(msg, client){
                     //valid new hi
                     console.log("  valid \"hi\" from " + msg.member.user.tag)
                     console.log("  updating data")
-                    lastHi = msg.createdTimestamp
-                    nextHi = lastHi + DAY_IN_MILLIS;
-                    lastUser = msg.member.user.id;
-                    if(lastUser != client.user.id){
+                    data.lastHi = msg.createdTimestamp
+                    data.nextHi = data.lastHi + DAY_IN_MILLIS;
+                    data.lastUser = msg.member.user.id;
+                    if(data.lastUser != client.user.id){
                         //if the last hi wasn't from the bot, set up a ping for 7 days
                         console.log("  resetting ping timer")
-                        clearTimeout(pingHi);
-                        pingHi = setTimeout(function () {
+                        clearTimeout(pingArray[data.server]);
+                        pingArray[data.server] = setTimeout(function () {
                             var channel = msg.channel;
                             channel.send("hi");
                             hasPinged = true;
                         }, DAY_IN_MILLIS * 7);
                     }
+                    db.setHi(data);
                 }
             }
         } else {
